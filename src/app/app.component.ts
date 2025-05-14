b2480d17d26d150e320cee94c556bec6048b504d
Katerine Florez <kflorez@makesysrems.com.co>
2025-05-14 16:14:23 -0500

    Se avanza en header y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conocemeAPP';
+  hideHeaderFooter = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.hideHeaderFooter = ['/', '/home', '/login'].includes(event.urlAfterRedirects);
+      }
+    });
+  }
 }
